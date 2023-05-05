196dda68cc542da15732105a64a9983f1d63bff9
Nancia <rajerisonnancia@gmail.com>
2023-05-05 11:38:54 +0300

    Update liste.blade.php

--- resources/views/front_office/liste.blade.php
+++ resources/views/front_office/liste.blade.php
@@ -1,1 +1,16 @@
-<h1>Hello</h1>
+<html>
+    <table width="800" border="1">
+        <tr>
+            <th>Date</th>
+            <th>Titre</th>
+            <th>Resume</th>
+        </tr>
+        <?php foreach($info as $row){ ?>
+        <tr>
+            <td><?php echo $row['date']; ?></td>
+            <td><?php echo $row['titre']; ?></td>
+            <td><?php echo $row['resume']; ?></td>
+        </tr>
+        <?php } ?>
+    </table>
+</html>
